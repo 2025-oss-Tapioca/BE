976af9143e248602c22985310bf2e923c2db33e5
top6543top <top6543top@gamil.com>
2025-07-14 15:58:52 +0900

    [feat] CI/CD with Docker Configuration

---
+++ src/main/java/com/tapioca/BE/TestController.java
@@ -0,0 +1,14 @@
+package com.tapioca.BE;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("/")
+    public String home(){
+        return "CI/CD perpect";
+    }
+}
