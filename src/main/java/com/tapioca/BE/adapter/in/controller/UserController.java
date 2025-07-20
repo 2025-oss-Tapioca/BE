f6ad017495f5b088ef9907af5a6603c1a953a907
top6543top <top6543top@gamil.com>
2025-07-20 23:12:35 +0900

    [feat] 로그인 회원 가입 로직에 따른 controller mapper Jwt 설정 구현 ErrorCode 구현

---
+++ src/main/java/com/tapioca/BE/adapter/in/controller/UserController.java
@@ -0,0 +1,33 @@
+package com.tapioca.BE.adapter.in.controller;
+
+import com.tapioca.BE.application.dto.request.user.LoginRequestDto;
+import com.tapioca.BE.application.dto.request.user.SignUpRequestDto;
+import com.tapioca.BE.config.common.CommonResponseDto;
+import com.tapioca.BE.domain.port.in.usecase.user.UserLoginUseCase;
+import com.tapioca.BE.domain.port.in.usecase.user.UserSignUpUseCase;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class UserController {
+    private final UserLoginUseCase userLoginUseCase;
+    private final UserSignUpUseCase userSignUpUseCase;
+
+    @PostMapping("/login")
+    public CommonResponseDto<?> userLogin(
+            @RequestBody LoginRequestDto loginRequestDto
+    ){
+        return CommonResponseDto.ok(userLoginUseCase.login(loginRequestDto));
+    }
+
+    @PostMapping("/signup")
+    public CommonResponseDto<?> userSignUp(
+            @RequestBody SignUpRequestDto signRequestDto
+    ){
+        userSignUpUseCase.signUp(signRequestDto);
+        return CommonResponseDto.created(null);
+    }
+}
