b1a2835dfd38022e828fcca44154e7c1a67577e5
top6543top <top6543top@gamil.com>
2025-07-22 17:16:25 +0900

    [remove] GPT 기능 삭제

--- src/main/java/com/tapioca/BE/domain/port/in/usecase/gpt/GptRequestUseCase.java
+++
@@ -1,8 +0,0 @@
-package com.tapioca.BE.domain.port.in.usecase.gpt;
-
-import com.tapioca.BE.application.dto.request.gpt.GptRequestDto;
-import com.tapioca.BE.application.dto.response.gpt.GptResponseDto;
-
-public interface GptRequestUseCase {
-    GptResponseDto gptRequest(GptRequestDto requestDto);
-}
