8de6ac43f26f0e9c45c9d77849a3cd445d478057
JAEHYUK LEE <107346845+jaehyuk2-lee@users.noreply.github.com>
2025-07-22 17:08:36 +0900

    [feat] domain model ERD, Diagram 내용 추가

---
+++ src/main/java/com/tapioca/BE/domain/model/Diagram.java
@@ -0,0 +1,5 @@
+package com.tapioca.BE.domain.model;
+
+public class Diagram {
+
+}
--- src/main/java/com/tapioca/BE/domain/model/ErdList.java
+++
@@ -1,9 +0,0 @@
-package com.tapioca.BE.domain.model;
-
-import java.util.UUID;
-
-public class ErdList {
-    private final UUID id;
-    private final UUID erdId;
-    private final String
-}
