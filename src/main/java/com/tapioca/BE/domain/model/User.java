dafc8605ce90e34fcf13c97c80c68203a1516976
top6543top <top6543top@gamil.com>
2025-07-29 16:17:42 +0900

    [fix] dto, entity, domain 수정

--- src/main/java/com/tapioca/BE/domain/model/User.java
+++ src/main/java/com/tapioca/BE/domain/model/User.java
@@ -11,30 +11,30 @@ public class User {
     private final UUID id;
     private final String email;
     private final String name;
-    private final String userId;
+    private final String loginId;
     private String password;
 
     public User(
             UUID id, String email,
-            String name, String userId,
+            String name, String loginId,
             String password
     ){
         this.id = id;
         this.email = email;
-        this.userId = userId;
+        this.loginId = loginId;
         this.name = name;
         this.password = password;
     }
 
     public User(
             UUID id,
-            String name, String userId,
+            String loginId,
             String password
     ){
         this.id = id;
         this.email = null;
-        this.userId = userId;
-        this.name = name;
+        this.name = null;
+        this.loginId = loginId;
         this.password = password;
     }
 
