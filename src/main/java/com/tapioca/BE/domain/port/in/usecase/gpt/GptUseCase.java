c1bcf8197957e4f82b5d570a1f22e2c0e5b4e49a
top6543top <top6543top@gamil.com>
2025-07-29 14:58:09 +0900

    [feat] 유스케이스 및 서비스 로직 작성

--- src/main/java/com/tapioca/BE/domain/port/in/usecase/gpt/GptUseCase.java
+++ src/main/java/com/tapioca/BE/domain/port/in/usecase/gpt/GptUseCase.java
@@ -2,9 +2,15 @@ package com.tapioca.BE.domain.port.in.usecase.gpt;
 
 import com.tapioca.BE.application.dto.request.gpt.GptRequestDto;
 import com.tapioca.BE.application.dto.response.gpt.GptResponseDto;
+import com.tapioca.BE.config.security.CustomUserDetails;
 import org.springframework.http.ResponseEntity;
+
+import java.util.UUID;
 
 
 public interface GptUseCase {
-    public ResponseEntity<GptResponseDto> gptRequest(GptRequestDto GptRequestDto);
+    public Object gptRequest(
+            GptRequestDto GptRequestDto, UUID teamId
+            ,String authorizationHeader
+    );
 }
