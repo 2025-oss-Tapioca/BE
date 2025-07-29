9453b3f3932971c8cf98435c0e2006ef7411ac1f
top6543top <top6543top@gamil.com>
2025-07-29 16:18:10 +0900

    [fix] repository 수정

--- src/main/java/com/tapioca/BE/domain/port/out/repository/user/UserRepository.java
+++ src/main/java/com/tapioca/BE/domain/port/out/repository/user/UserRepository.java
@@ -3,10 +3,11 @@ package com.tapioca.BE.domain.port.out.repository.user;
 import com.tapioca.BE.adapter.out.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository {
-    public UserEntity findByUserId(String userId);
+    public UserEntity findByLoginId(String loginId);
     public UserEntity save(UserEntity userEntity);
-    public boolean existsByUserId(String userId);
+    public boolean existsByLoginId(String loginId);
 }
