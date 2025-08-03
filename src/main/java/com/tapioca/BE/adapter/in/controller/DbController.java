9d45a3b67e2a1253dc5530564a440caafdd906cd
Cho Suhyeon <ckc020506@gmail.com>
2025-08-03 19:15:38 +0900

    [feat] DB 등록 컨트롤러 생성

---
+++ src/main/java/com/tapioca/BE/adapter/in/controller/DbController.java
@@ -0,0 +1,26 @@
+package com.tapioca.BE.adapter.in.controller;
+
+import com.tapioca.BE.application.dto.request.db.RegisterRequestDto;
+import com.tapioca.BE.config.common.CommonResponseDto;
+import com.tapioca.BE.config.security.CustomUserDetails;
+import com.tapioca.BE.domain.port.in.usecase.db.DbRegisterUseCase;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class DbController {
+    private final DbRegisterUseCase dbRegisterUseCase;
+
+    @PostMapping("/api/signup/deploy/db")
+    public CommonResponseDto<?> dbRegister(
+            @AuthenticationPrincipal CustomUserDetails user,
+            @RequestBody RegisterRequestDto registerRequestDto
+    ) {
+        dbRegisterUseCase.register(user.getUserId(), registerRequestDto);
+        return CommonResponseDto.ok(null);
+    }
+}
