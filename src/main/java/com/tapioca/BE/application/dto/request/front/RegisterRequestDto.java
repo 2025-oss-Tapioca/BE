c5a84a7bd36d29263cf0ba402427621c4fad4e62
Cho Suhyeon <ckc020506@gmail.com>
2025-08-05 17:55:40 +0900

    Front dto 수정

--- src/main/java/com/tapioca/BE/application/dto/request/front/RegisterRequestDto.java
+++ src/main/java/com/tapioca/BE/application/dto/request/front/RegisterRequestDto.java
@@ -3,7 +3,6 @@ package com.tapioca.BE.application.dto.request.front;
 import java.util.UUID;
 
 public record RegisterRequestDto(
-        UUID teamId,
         String ec2Host,
         String entryPoint,
         String os,
