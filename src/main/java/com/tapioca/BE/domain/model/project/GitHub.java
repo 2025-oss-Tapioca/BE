08739fb5b826453cbeb806850cda6ad3cc9f3de0
Cho Suhyeon <ckc020506@gmail.com>
2025-08-07 23:47:30 +0900

    [feat] domain model teamCode 추가

--- src/main/java/com/tapioca/BE/domain/model/project/GitHub.java
+++ src/main/java/com/tapioca/BE/domain/model/project/GitHub.java
@@ -1,21 +1,24 @@
 package com.tapioca.BE.domain.model.project;
 
 public class GitHub {
+    private final String teamCode;
     private final String repoUrl;
     private final boolean isPrivate;
     private final String accessToken;
     private final String defaultBranch;
 
     public GitHub(
-            String repoUrl, boolean isPrivate,
+            String teamCode, String repoUrl, boolean isPrivate,
             String accessToken, String defaultBranch
     ) {
+        this.teamCode = teamCode;
         this.repoUrl = repoUrl;
         this.isPrivate = isPrivate;
         this.accessToken = accessToken;
         this.defaultBranch = defaultBranch;
     }
 
+    public String getTeamCode() { return teamCode; }
     public String getRepoUrl() { return repoUrl; }
     public boolean isPrivate() { return isPrivate; }
     public String getAccessToken() { return accessToken; }
