f177b8aeb57db6185bb7f349221d0086921488bd
top6543top <top6543top@gamil.com>
2025-08-16 01:15:14 +0900

    [Dev] WebConfig 수정

--- src/main/java/com/tapioca/BE/config/cors/WebConfig.java
+++ src/main/java/com/tapioca/BE/config/cors/WebConfig.java
@@ -2,24 +2,30 @@ package com.tapioca.BE.config.cors;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
+
     @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173",
-                                "https://ossp6-front.vercel.app",
-                                "https://ossp6-front-4796s-projects.vercel.app",
-                                "https://ossp6-front-git-main-4796s-projects.vercel.app"
-                        )
-                        .allowedMethods("*");
-            }
-        };
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // 모든 경로 허용
+                .allowedOrigins(
+                        "http://localhost:5173",
+                        "https://ossp6-front.vercel.app",
+                        "https://ossp6-front-4796s-projects.vercel.app",
+                        "https://ossp6-front-git-main-4796s-projects.vercel.app",
+                        "https://3.37.148.82"
+                )
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메서드
+                .allowedHeaders("*") // 모든 요청 헤더 허용
+                .allowCredentials(true); // 쿠키/인증 정보 허용
     }
 }
